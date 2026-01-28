712ac631f450ceddea662a0fd9c98885835cf369
niaz abir <niazabir55@gmail.com>
2026-01-28 15:30:54 +0600

    client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       <Banner />
       <Services />
       {/* <Pricing /> */}
-      <Testimonial />
+      {/* <Testimonial /> */}
       <ChooseUs />
       {/* <Portfolio /> */}
       <ConnectCompany />
